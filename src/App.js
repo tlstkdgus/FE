cdf35ee95dae131b3dc2adb8cca62f624bb6f42b
tlstkdgus <tlstkdgus9@naver.com>
2025-05-06 21:00:16 +0900

    feat: 전역 배경 셋팅

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import RouterComponent from "./router";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import styled from "styled-components";
+import Mobile from "./styles/Mobile";
+
+const Container = styled.div`
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  background-color: aliceblue;
+  background-attachment: fixed;
+`;
 
 function App() {
   return (
-    <>
-      <RouterComponent />;
-    </>
+    <Container>
+      <GlobalStyle />
+      <Mobile>
+        <RouterComponent />
+      </Mobile>
+    </Container>
   );
 }
 
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+import { HiOutlineXCircle } from "react-icons/hi";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import { GrPowerCycle } from "react-icons/gr";
+
+const NavBar = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 8px 16px;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid var(--line);
+  background: var(--white);
+`;
+
+const NavItem = styled.div`
+  display: flex;
+  width: 94px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  gap: 8px;
+  flex-shrink: 0;
+`;
+
+export default function NavBarComponent() {
+  return (
+    <NavBar className="body-medium-small">
+      <NavItem>
+        <HiOutlineXCircle size={20} color="var(--brand)" />
+        수강한 과목
+        <br /> 제외
+      </NavItem>
+      <NavItem>
+        <AiOutlineCheckCircle size={20} color="var(--brand)" />
+        필수 과목
+        <br /> 설정
+      </NavItem>
+      <NavItem>
+        <GrPowerCycle size={20} color="var(--brand)" />
+        재수강 과목
+        <br /> 설정
+      </NavItem>
+    </NavBar>
+  );
+}
