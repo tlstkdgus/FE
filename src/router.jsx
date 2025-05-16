992ba0d9548bfbef36b2b8dc2e672151b254cc60
choib1ue <choiblue1012@naver.com>
2025-05-16 23:38:13 +0900

    feat: 회원가입 완료 UI 구현

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import Create from "./pages/Create";
@@ -9,21 +9,21 @@ import MyPage from "./pages/MyPage";
 import Retake from "./pages/Retake";
 import SignUp from "./pages/SignUp";
 import Setting from "./pages/Setting";
+import SignUpComplete from "./pages/SignUpComplete";
 
-const RouterComponent = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Setting />} />
-      <Route path="/main" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/create" element={<Create />} />
-      <Route path="/essential" element={<Essential />} />
-      <Route path="/exclude" element={<Exclude />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/retake" element={<Retake />} />
-    </Routes>
-  );
-};
+const RouterComponent = () => (
+  <Routes>
+    <Route path="/" element={<Setting />} />
+    <Route path="/main" element={<Main />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<SignUp />} />
+    <Route path="/signup-complete" element={<SignUpComplete />} />
+    <Route path="/create" element={<Create />} />
+    <Route path="/essential" element={<Essential />} />
+    <Route path="/exclude" element={<Exclude />} />
+    <Route path="/mypage" element={<MyPage />} />
+    <Route path="/retake" element={<Retake />} />
+  </Routes>
+);
 
 export default RouterComponent;
