fa94733fec0b5f54e9ca1a3a8b6949b30c145ad2
tlstkdgus <tlstkdgus9@naver.com>
2025-05-18 23:37:02 +0900

    Feat: 라우팅 설정

--- src/router.jsx
+++ src/router.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
-import Create from "./pages/Create";
 import Essential from "./pages/Essential";
 import Exclude from "./pages/Exclude";
 import MyPage from "./pages/MyPage";
 import Retake from "./pages/Retake";
 import SignUp from "./pages/SignUp";
 import Setting from "./pages/Setting";
-
+import Create from "./pages/Create";
+import Day from "./pages/Day";
+import Credit from "./pages/Credit";
+import Combination from "./pages/Combination";
+import Result from "./pages/Result";
+import DetailedCredit from "./pages/DetailedCredit";
 const RouterComponent = () => {
   return (
     <Routes>
@@ -17,11 +21,16 @@ const RouterComponent = () => {
       <Route path="/main" element={<Main />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/create" element={<Create />} />
       <Route path="/essential" element={<Essential />} />
       <Route path="/exclude" element={<Exclude />} />
       <Route path="/mypage" element={<MyPage />} />
       <Route path="/retake" element={<Retake />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/day" element={<Day />} />
+      <Route path="/credit" element={<Credit />} />
+      <Route path="/combination" element={<Combination />} />
+      <Route path="/detailedcredit" element={<DetailedCredit />} />
+      <Route path="/result" element={<Result />} />
     </Routes>
   );
 };
